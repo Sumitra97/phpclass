c2e0f5cba53772522a09773ee156eb2374b567e8
Sumitra Adhikari <adhikarisumitra003@gmail.com>
2018-03-05 17:18:07 +0545

    Added bootstrap framework.

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My first project</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+</head>
+<body>
+<h1> My first project </h1>
+
+<h3>Courses</h3>
+<ul>
+	<li>Basic Php</li>
+	<li>Advanced Php</li>
+	<li>Laravel 5</li>
+</ul>
+</body>
+</html>
